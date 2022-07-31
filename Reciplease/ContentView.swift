dad994d253d73bc2fa311a9f1a6ce01cf26f99cf
OGGERO Paul <paul.oggero@softeam.fr>
2022-07-31 22:02:26 +0200

    first commit

---
+++ Reciplease/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Reciplease
+//
+//  Created by Paul Oggero on 31/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Reciplease/RecipleaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipleaseApp.swift
+//  Reciplease
+//
+//  Created by Paul Oggero on 31/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct RecipleaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
